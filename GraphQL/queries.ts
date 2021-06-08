ce7ac8b97624164ae41cbde4de7dc4f0b06ab728
Colton <czpemberton@gmail.com>
2021-06-08 12:39:54 -0700

    apollo set up with queries

---
+++ GraphQL/queries.ts
@@ -0,0 +1,66 @@
+import { gql } from '@apollo/client'
+
+export const GET_LATEST = gql`
+  query latest($playlistId: String!, $key: String!) {
+    getLatest(playlistId: $playlistId, key: $key) {
+      items {
+        id
+        snippet {
+          title
+          description
+          thumbnails {
+            maxres {
+              url
+            }
+          }
+          resourceId {
+            videoId
+          }
+        }
+      }
+    }
+  }
+`
+
+export const GET_PLAYLIST = gql`
+  query playlist($playlistId: String!, $key: String!) {
+    getPlaylist(playlistId: $playlistId, key: $key) {
+      items {
+        id
+        snippet {
+          title
+          description
+          thumbnails {
+            maxres {
+              url
+            }
+          }
+          resourceId {
+            videoId
+          }
+        }
+      }
+    }
+  }
+`
+
+export const GET_PLAYLISTS = gql`
+  query playlists($channelId: String!, $key: string!) {
+    getPlaylists(channelId: $channelId, key: $key) {
+      items {
+        id
+        snippet {
+          title
+        }
+      }
+    }
+  }
+`
+
+export const GET_VIDEO = gql`
+  query video($id: String!, $key: string!) {
+    getVideo(id: $id, key: $key) {
+      id
+    }
+  }
+`
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,28 @@
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
+
 import { AuthProvider } from '../components/auth/AuthContext'
+import { SignIn } from '../components/auth/SignIn'
+
 import '../styles/index.scss'
+
+export const client = new ApolloClient({
+  uri: 'http://localhost:4000/',
+  cache: new InMemoryCache(),
+})
 
 function MyApp({ Component, pageProps }) {
   return (
-    <AuthProvider>
-      <Component {...pageProps} />
-    </AuthProvider>
+    <ApolloProvider client={client}>
+      <AuthProvider>
+        <header className="container">
+          <a href="/">
+            <h1>Traveling Mai</h1>
+          </a>
+          <SignIn />
+        </header>
+        <Component {...pageProps} />
+      </AuthProvider>
+    </ApolloProvider>
   )
 }
 
