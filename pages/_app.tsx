52cdc461d88a96c3b81efc8aef2b79e101a550a8
Colton <czpemberton@gmail.com>
2021-06-14 16:13:24 -0700

    remove google tag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,4 @@
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
-import Head from 'next/head'
 
 import { AuthProvider } from '../components/auth/AuthContext'
 import { SignIn } from '../components/auth/SignIn'
@@ -13,25 +12,17 @@ export const client = new ApolloClient({
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
-      <Head>
-        <meta
-          name="google-site-verification"
-          content="MP-dP9mdVxusdz3LHLPYMAKOOzpULP7IVVjvKpXD9_4"
-        />
-      </Head>
-      <ApolloProvider client={client}>
-        <AuthProvider>
-          <header className="container">
-            <a href="/">
-              <h1>Traveling Mai</h1>
-            </a>
-            <SignIn />
-          </header>
-          <Component {...pageProps} />
-        </AuthProvider>
-      </ApolloProvider>
-    </>
+    <ApolloProvider client={client}>
+      <AuthProvider>
+        <header className="container">
+          <a href="/">
+            <h1>Traveling Mai</h1>
+          </a>
+          <SignIn />
+        </header>
+        <Component {...pageProps} />
+      </AuthProvider>
+    </ApolloProvider>
   )
 }
 
